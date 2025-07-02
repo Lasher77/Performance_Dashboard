8dc5b29d07bfd2545f44c39e73a750575ea5b788
Lasher77 <sven.winkler@gmx.net>
2025-07-02 08:50:35 +0200

    Add web interface scaffold

---
+++ api/analysis.py
@@ -0,0 +1,10 @@
+# Simple analysis module
+
+def get_membership_metrics():
+    """Return aggregated membership metrics."""
+    # Placeholder implementation
+    return {
+        'active_members': 120,
+        'inactive_members': 30,
+        'total_members': 150
+    }
---
+++ api/app.py
@@ -0,0 +1,3 @@
+from .main import app
+
+# For `uvicorn api.app:app --reload`
